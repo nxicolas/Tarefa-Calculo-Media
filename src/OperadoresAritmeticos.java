3f7c6dd2f8cb8bc134960557e47cc3e9bdef9099
nxicolas <nicolasscp15@gmail.com>
2024-10-18 10:12:54 -0300

    tarefa

---
+++ src/OperadoresAritmeticos.java
@@ -0,0 +1,21 @@
+public class OperadoresAritmeticos {
+
+    public static void main(String args[]){
+        int num1 = 17;
+        int num2 = 38;
+
+        int num3 = num1 * num2;
+        int num4 = num2 / num1;
+        int num5 = (num1 + num2 + num3 + num4)/4;
+
+        System.out.println(num5);
+
+
+
+
+
+
+
+
+    }
+}
